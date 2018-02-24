dbd07e79a0ce58ee1fe1d74d19cc73c054bcea7d
Nick Volkov <aid13@mail.ru>
2018-02-24 23:26:46 +0300

    Initial save of functional script

---
+++ Image_Sorting.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Arguments support, under construction
+# passed_args=$1
+#
+# if [[ -n $passed_args ]]
+# then
+# echo "You passed these arguments: $passed_args. Arguments support is under construction."
+# fi
+
+#Make the desired folder choice interactive later on
+FOLDER=./
+cd $FOLDER
+#clean up the error list
+rm -f err_list
+rm -f saved_list.txt
+touch err_list
+touch saved_list.txt
+
+#Read image-only filenames and write them to the saved_list.txt
+ls *jpg 2>/dev/null
+if [[ $? == 0 ]]
+then ls -1 *jpg 2>>err_list >> saved_list.txt
+fi
+ls *jpeg 2>/dev/null
+if [[ $? == 0 ]]
+then ls -1 *jpeg 2>>err_list >> saved_list.txt
+fi
+ls *png 2>/dev/null
+if [[ $? == 0 ]]
+then ls -1 *png 2>>err_list >> saved_list.txt
+fi
+ls *gif 2>/dev/null
+if [[ $? == 0 ]]
+then ls -1 *gif 2>>err_list >> saved_list.txt
+fi
+
+if [[ $(head err_list) == '' ]]
+then
+  echo "The list of file names is saved at saved_list.txt in the images folder."
+else
+  echo "There was an error while saving the list of file names: $(head err_list)"
+fi
+
+#bug: hyphen at the start of the names seems to break the script!
+#replace symbols, numbers and extensions with newlines
+sed -i -r -e 's/\.(jpg|jpeg|png|gif)//gi' saved_list.txt #strip extensions
+sed -i -r -e 's/(\-|\_|[0-9])/\n/gi' saved_list.txt #break by symbols and numbers delimeters to newlines
+sed -i -r -e '/^\s*$/d' saved_list.txt #clean up whitespaces
+sed -i -r -e 's/./\L&/gi' saved_list.txt #lowercase all letters
+sed -i -r -e '/\b.{1,2}\b/d' saved_list.txt #remove 1-2 lettered words
+
+#sorting and building frequency list
+sort -o saved_list.txt saved_list.txt #sort lines into alphabetical order
+uniq -c -d saved_list.txt freq_list.txt #build frequency list and remove unique items
+sort -n -r -o saved_list.txt freq_list.txt #sort words based on frequencies
+
+#create folders based on the generated freq list
+INCREM=1
+while [[ $(sed -n -e ${INCREM}p saved_list.txt) ]]; do
+  mkdir -p $(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi')
+  INCREM=$((INCREM+1))
+done
+
+#sort images into folders starting with the most frequent filenames
+INCREM=1
+while [[ $(sed -n -e ${INCREM}p saved_list.txt) ]]; do
+  mv -i -v -t $(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi') *$(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi')*.jpg
+  mv -i -v -t $(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi') *$(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi')*.jpeg
+  mv -i -v -t $(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi') *$(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi')*.png
+  mv -i -v -t $(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi') *$(sed -n -e ${INCREM}p saved_list.txt | sed -r -e 's/([0-9]|\s)//gi')*.gif
+  INCREM=$((INCREM+1))
+done
+#clean up empty directories, that have lost the contest (e.g. animal folder over wolf folder in animal-wolf.jpg due to higher frequency of animal folder)
+ls --file-type --ignore="*.*" | grep "/" > deleted.txt #keep a list of all items that were removed for future optimizations
+rm -d -f $(ls --file-type --ignore="*.*" | grep "/") #WARNING! EXPERIMENTAL, COULD LOSE FILES
+#delete all temporary files
+rm -f freq_list.txt
+rm -f saved_list.txt
+rm -f err_list
+#Return to the previous current directory
+cd -
